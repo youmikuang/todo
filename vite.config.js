6b100ad40d7307604de33818501e0f800674227e
YMK <laragh@163.com>
2026-01-28 06:57:07 +0800

    change mobile show

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,12 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    port: 5173,
+    host: true,
+    cors: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
 })
